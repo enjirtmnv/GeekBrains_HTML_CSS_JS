af5038d2541275de914b6a591f0d3d81c01a232a
enjirtmnv <shkensinio@gmail.com>
2020-01-30 18:44:50 +0300

    attempt to make media with js

---
+++ 02_coffee/js/script.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", forPhone, false);
+//
+// let headerParallax = document.querySelector(".headerParallax");
+//
+// function forPhone() {
+//     if (window.matchMedia("(max-width: 1024px)").matches) {
+//         headerParallax.classList.remove(".headerParallax");
+//     } else {
+//         headerParallax.classList.add(".headerParallax");
+//     }
+// }
