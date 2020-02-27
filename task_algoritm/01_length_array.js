66699b2564e4696a0be81f0ede38e72da48c90f4
enjirtmnv <shkensinio@gmail.com>
2020-02-27 17:19:42 +0300

    fix

--- task_algoritm/01_length_array.js
+++ task_algoritm/01_length_array.js
@@ -1,21 +1,26 @@
-let arr1 = [0,0,0];
-let arr2 = [1,0,1,1,1,0];
-let arr3 = [1,0,1,1,1,1];
-let arr4 = [1,1,1,1,1,1];
-let arr5 = [];
+let myArr = [ [0,0,0], [1,0,1,1,1,0], [1,0,1,1,1,1], [1,1,1,1,1,1], [] ];
+
+// let myArr1 = [0,0,0];
+// let myArr2 = [1,0,1,1,1,0];
+// let myArr3 = [1,0,1,1,1,1];
+// let myArr4 = [1,1,1,1,1,1];
+// let myArr5 = [];
 
 
-function lengthArr() {
+function lengthArr(arr) {
 
-    let current = 0;
-
-        for(let i of arr4){
-                if (i > 0 ){
-                    current = current +1;
-                }
+    let current = 0;                     //O(1)
+        for(let i of arr){               //O(n)
+            if (i > 0 ){                 //O(1)
+                current = current +1;     //O(1)
+            }
         }
-
-    console.log(current);
+    return current;                     //O(1)
 }
 
-lengthArr();
+
+// console.log(lengthArr(myArr2));
+
+for (let j of myArr){
+    console.log(lengthArr(j));     //O(n)
+}
