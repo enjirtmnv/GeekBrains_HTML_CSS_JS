67ecf4eed75a30e1e5bd7de9a9c8aff16ef6c567
enjirtmnv <shkensinio@gmail.com>
2020-03-06 23:10:08 +0300

    add task

--- task_algoritm/05_recursion.js
+++ task_algoritm/05_recursion.js
@@ -197,3 +197,17 @@ function printListCycle(list) {
 }
 
 printListCycle(list);
+
+
+function fibonacci(n){
+    if (n <= 1){
+        return n
+    } else {
+        return (fibonacci(n-1)+fibonacci(n-2))
+    }
+}
+
+console.log(fibonacci(7));
+
+// 0 1 2 3 4 5 6 7
+// 0 1 1 2 3 5 8 13
