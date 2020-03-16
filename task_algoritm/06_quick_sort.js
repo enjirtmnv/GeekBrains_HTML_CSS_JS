63c4b1501ee2dd6d6084b7b59ab31c928cb81cff
enjirtmnv <shkensinio@gmail.com>
2020-03-16 19:26:21 +0300

    add task quicksort, where pivot = middle

--- task_algoritm/06_quick_sort.js
+++ task_algoritm/06_quick_sort.js
@@ -1,4 +1,4 @@
-//Быстрая сортировка через for
+//Быстрая сортировка через for (опорный - первый элемент)
 function quickSort(arr) {
     if (arr.length === 0){
         return [];
@@ -14,6 +14,7 @@ function quickSort(arr) {
         } else {
             greater[greater.length] = arr[i];
         }
+        var ggg = 3;
     }
     return quickSort(less).concat(pivot, quickSort(greater));
 }
@@ -23,7 +24,7 @@ myArr = [2,55,677,1,-98,34,76,1343,560,88,13,-7];
 console.log(quickSort(myArr));
 
 
-//Быстрая сортировка через forEach
+//Быстрая сортировка через forEach (опорный - первый элемент)
 function quickSort2(arr) {
     if (arr.length === 0){
         return [];
@@ -33,8 +34,9 @@ function quickSort2(arr) {
     var greater = [];
     var pivot = arr[0];
 
-    arr.forEach(function (item, i, arr){
+    arr.shift();
 
+    arr.forEach(function (item){
         if (item < pivot){
             less[less.length] = item;
         } else {
@@ -48,3 +50,54 @@ function quickSort2(arr) {
 myArr2 = [2,55,677,1,-98,34,76,1343,560,88,13,-7];
 
 console.log(quickSort2(myArr2));
+
+//Быстрая сортировка (опорный - из середины)
+function swap(items, firstIndex, secondIndex) {
+    const temp = items[fisrtIndex];           //temp - temporary (временный, не нужный)
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+function partition(items, left, right) {
+    var pivot = items[Math.floor( (left+right)/2 )];
+    var i = left;
+    var j = right;
+
+   while (i <= j){
+
+       while (items[i] < pivot){
+           i++;
+       }
+       while (items[j] > pivot){
+           j--;
+       }
+       if ( i <= j){
+           swap(items,i,j);
+           i++;
+           j--;
+       }
+   }
+   return i;
+}
+
+
+function quickSort3(items, left, right) {
+    var index;
+
+    if (items.length > 1){
+        index = partition(items, left,right);
+
+        if (left < index - 1){
+            quickSort3(items, left, index - 1)
+        }
+
+        if (left < right){
+            quickSort3(items, left, right)
+        }
+    }
+    return items
+}
+
+myArr3 = [2,55,677,1,-98,34,76,1343,560,88,13,-7];
+
+console.log(quickSort3(myArr3));
